2d8840ae8ad071b4fee4752be272e4294be0c871
Mat Fukano <fukano.mat@gmail.com>
2025-01-03 02:11:05 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Simple Markdown Document Reader
+
+## What is this?
+
+SMDR is a small utility to quickly check the general formatting of a markdown file.
+
+It's inspired by grip, which does a lot more than this, but also leverages the browser.
+I tend to have oceans of tabs open in my browser when I'm coding because
+my memory is **notoriously poor** (except for specific things;
+ask my friends about my ability to be an encyclopedia for powerups in roguelike games).
+
+Instead, it constructs a webview and injects it with local Github Markdown stylesheets
+(c.o. [sindresorhus/github-markdown-css](https://github.com/sindresorhus/github-markdown-css)).
+
+Ideally it will be a single file executable with basic options for light
+and dark mode.
+
+## Installation
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun view [markdown file path]
+```
+
+To build:
+
+```bash
+bun build:dev
+```
+
+I also recommend aliasing it, since it can be annoying to type options - especially
+for a binary that has _exactly one_ right now, but my dream is to potentially
+make this bidirectional and a seamless click-in editor. (far-flung, maybe)
+
+I'm using `zsh` with a dedicated `.zsh_aliases` file, so my setup
+looks kinda like this:
+
+```bash
+alias smdr="/Users/mat/programs/smdr -f"
+```
+
+but if you're compiling it yourself you can put the bin anywhere and
+change the path easily. All of the relevant stylesheets in the project
+should be aliased and embedded into the executable.
+
+## Shoutout Bun for being awesome
+
+This project was created using `bun init` in bun v1.1.30.
+[Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,106 @@
+import index from "./index.html" with { type: "file" };
+import globalStyles from "./css/style.css" with { type: "file" };
+import ghDark from "./css/github-dark.css" with { type: "file" };
+import ghLight from "./css/github-light.css" with { type: "file" };
+
+import { Webview } from "webview-bun";
+import { parseArgs } from "util";
+import { marked } from "marked";
+
+const INDEX = Bun.file(index);
+const GLOBAL_STYLES = await Bun.file(globalStyles).text();
+const GH_DARK = await Bun.file(ghDark).text();
+const GH_LIGHT = await Bun.file(ghLight).text();
+
+function getPathFromArgs() {
+  const { values, positionals } = parseArgs({
+    args: Bun.argv,
+    options: {
+      f: {
+        type: "string",
+      },
+    },
+    strict: true,
+    allowPositionals: true,
+  });
+
+  console.log(values);
+  console.log(positionals);
+
+  console.log(positionals[2]);
+
+  const path = values.f ? values.f : null;
+
+  return path;
+}
+
+async function fileTextFromPath(path?: string) {
+  const html = INDEX;
+
+  if (!path) {
+    return await html.text();
+  }
+
+  const fileMarkdown = Bun.file(path);
+  const fileMarkdownText = await fileMarkdown.text();
+  const fileMarkup = marked.parse(fileMarkdownText);
+
+  const fileText =
+    fileMarkup.toString().length === 0 ? await html.text() : fileMarkup;
+
+  // console.log(`<fileTextFromPath()> [fileText]: ${fileText}`);
+  return fileText;
+}
+
+function buildStyledMarkup(markup: string) {
+  const styledMarkup = `
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Hey There Delilah</title>
+      <style>
+        ${GLOBAL_STYLES}
+        ${GH_DARK}
+      </style>
+    </head>
+    <body>
+      <main>
+        <article class="markdown-body">
+          ${markup}
+        </article>
+      </main>
+    </body>
+  </html>
+`;
+
+  /*
+    console.log(`<buildStyledMarkup()> {styledMarkup}: 
+${styledMarkup}`);
+  */
+
+  return styledMarkup;
+}
+
+async function runWebview(path: string | null) {
+  if (!path) {
+    // set different HTML here
+    console.warn(`Error: values doesn't contain a flag`);
+  }
+
+  const fileTextToRender = await fileTextFromPath(path!);
+  const styledMarkup = buildStyledMarkup(fileTextToRender);
+
+  const webview = new Webview();
+  webview.setHTML(styledMarkup);
+  webview.run();
+}
+
+console.log(`check embedded files`);
+
+Bun.embeddedFiles.forEach((f, i) => {
+  console.log(`#${i} name: ${f.name}`);
+});
+
+const path = getPathFromArgs();
+await runWebview(path);
